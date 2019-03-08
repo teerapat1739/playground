d4ad692d5b4814ea2a64799a9e1e7a96d7bfc8e3
teerapat1739 <teerapat12345678@gmail.com>
2019-03-08 16:46:20 +0700

    restructure files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route } from "react-router-dom";
+
+// NAVIGATION
+import Nav from "./components/Nav";
+// PAGES
+import Home from "./pages/Home/HomePage";
+import About from "./pages/About/AboutPage";
+import Contact from "./pages/Contact/ContactPage";
+
+export const App = () => (
+  <div>
+    <Nav />
+    <div className="container">
+      <Route exact={true} path="/" component={Home} />
+      <Route exaxt path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+    </div>
+  </div>
+);
+
